561ea6a2a13cba3b436a623b417d690ce104941e
Francisco Suarez <franksg88@gmail.com>
2024-01-28 21:53:49 -0600

    Update .config/fish/config.fish

--- private_dot_config/fish/executable_config.fish
+++ private_dot_config/fish/executable_config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 fish_ssh_agent
-# pyenv init - | source
-# direnv hook fish | source
+pyenv init - | source
+direnv hook fish | source
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/opt/google-cloud-cli/path.fish.inc' ]; . '/opt/google-cloud-cli/path.fish.inc'; end
