dbd6dcd3a57f27b61c841f08e7bf97bfc71bf4a8
Francisco Suarez <franksg88@gmail.com>
2024-01-28 21:14:33 -0600

    Add .local/bin/autostart.sh Add .local/bin/bspwm-toggle-visibility Add .local/bin/clipboard-menu Add .local/bin/dmenu-password-generator Add .local/bin/favorites-apps-menu Add .local/bin/favorites-menu Add .local/bin/keyboard-swithcer-menu Add .local/bin/launch-polybar Add .local/bin/notify-suspend Add .local/bin/power-menu Add .local/bin/powersave-menu Add .local/bin/scratchpad-notes Add .local/bin/screenshots-menu Add .local/bin/sxhkd-help-menu Add .local/bin/utils-menu

---
+++ private_dot_local/bin/executable_autostart.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal &
+
+function restart {
+  if  ps x | rg  "$1" | awk '{print $1}'; then
+    pkill -f "$1"
+  fi
+  "$1 $2 $3 $4" &
+}
+restart dbus-launch --sh-syntax
+restart dunst
+restart betterlockscreen -w
+restart compfy
+eval "$(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)"
+rofi-polkit-agent &
+~/.local/bin/launch-polybar &
+greenclip daemon &
+xset -dpms
+xset s 840 900 # dim at 14 min and lock at 15 min
+xss-lock -n ~/.local/bin/notify-suspend  -- betterlockscreen -l dimblur &
+sleep 2s;restart udiskie -as
+
+if [ -f "$HOME/.local-machine" ] ; then
+  source "$HOME/.local-machine"
+fi
+notify-send "Welcome Back" &
