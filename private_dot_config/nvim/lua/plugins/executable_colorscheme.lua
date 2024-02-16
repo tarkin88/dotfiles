051127c795cc1fe075c11ae657827fc58aefa6e8
Francisco Suarez <franksg88@gmail.com>
2024-02-16 16:58:28 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/nvim/lua/plugins/executable_colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/executable_colorscheme.lua
@@ -1,15 +1,20 @@
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
+  "neanias/everforest-nvim",
+  version = false,
   lazy = false,
-  priority = 1000,
+  priority = 1000, -- make sure to load this before all the other start plugins
   opts = {
-
+    background = "soft",
+    transparent_background_level = 0,
+    italics = true,
+    disable_italic_comments = false,
+    on_highlights = function(hl, _)
+      hl["@symbol"] = { link = "@field" }
+    end,
   },
   config = function(_, opts)
-    local theme = require("rose-pine")
+    local theme = require("everforest")
     theme.setup(opts)
-    -- theme.load()
-    vim.cmd('colorscheme rose-pine-moon')
+    theme.load()
   end,
 }
