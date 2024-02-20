c92c2ef824f2d6383203a02a0fdc45f196af2166
Francisco Suarez <franksg88@gmail.com>
2024-02-19 23:00:44 -0600

    hypr

--- private_dot_config/nvim/lua/plugins/executable_colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/executable_colorscheme.lua
@@ -1,19 +1,49 @@
 return {
-  "neanias/everforest-nvim",
-  version = false,
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000, -- make sure to load this before all the other start plugins
   opts = {
-    background = "soft",
-    transparent_background_level = 0,
-    italics = true,
-    disable_italic_comments = false,
-    on_highlights = function(hl, _)
-      hl["@symbol"] = { link = "@field" }
-    end,
+    flavour = "frappe",
+    no_italic = false,         -- Force no italic
+    no_bold = false,           -- Force no bold
+    no_underline = false,      -- Force no underline
+    styles = {                 -- Handles the styles of general hi groups (see `:h highlight-args`):
+      comments = { "italic" }, -- Change the style of comments
+      conditionals = { "italic" },
+      loops = {},
+      functions = {},
+      keywords = { "bold" },
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = { "italic" },
+      operators = { "bold" },
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+    },
+    custom_highlights = function(colors)
+      return {
+        Comment = { fg = colors.flamingo },
+        TabLineSel = { bg = colors.pink },
+        CmpBorder = { fg = colors.surface2 },
+        Pmenu = { bg = colors.none },
+      }
+    end
   },
   config = function(_, opts)
-    local theme = require("everforest")
+    local theme = require("catppuccin")
     theme.setup(opts)
     theme.load()
   end,
