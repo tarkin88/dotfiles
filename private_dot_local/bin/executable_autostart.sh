461b9e53755e736b870fdc8640c1180094ea2c86
Francisco Suarez <franksg88@gmail.com>
2024-02-21 09:44:55 -0600

    Update .local/bin/autostart.sh Update .local/bin/screenshots-menu

--- private_dot_local/bin/executable_autostart.sh
+++ private_dot_local/bin/executable_autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 2560x1080 --pos 0x0 --rotate normal &
+xrandr --output eDP-1 --off --output HDMI-1 --mode 2560x1440 --pos 0x0 --rotate normal &
 
 function restart {
-  if  ps x | rg  "$1" | awk '{print $1}'; then
-    pkill -f $1
-  fi
-  $1 $2 $3 $4 &
+	if ps x | rg "$1" | awk '{print $1}'; then
+		pkill -f $1
+	fi
+	$1 $2 $3 $4 &
 }
 
 restart dbus-launch --sh-syntax
@@ -18,11 +18,12 @@ greenclip daemon &
 
 xset -dpms
 xset s 840 900 # dim at 14 min and lock at 15 min
-xss-lock -n ~/.local/bin/notify-suspend  -- betterlockscreen -l dimblur &
-sleep 2s;restart udiskie -as
+xss-lock -n ~/.local/bin/notify-suspend -- betterlockscreen -l dimblur &
+sleep 2s
+restart udiskie -as
 
-if [ -f "$HOME/.local-machine" ] ; then
-  source "$HOME/.local-machine"
+if [ -f "$HOME/.local-machine" ]; then
+	source "$HOME/.local-machine"
 fi
 
 notify-send "Welcome Back" &
