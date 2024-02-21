0392e4324b09e6452881420fab12b0ad028e855c
Francisco Suarez <franksg88@gmail.com>
2024-02-21 09:44:24 -0600

    Remove .config/qtile/__pycache__/config.cpython-311.pyc Remove .config/qtile/__pycache__/options.cpython-311.pyc Remove .config/qtile/config-original.py Remove .config/qtile/config.py Remove .config/qtile/core/__pycache__/__init__.cpython-311.pyc Remove .config/qtile/core/__pycache__/helper.cpython-311.pyc Remove .config/qtile/core/__init__.py Remove .config/qtile/core/keys.py Remove .config/qtile/core/helper.py Remove .config/qtile/.pylintrc Remove .config/qtile/options.py Remove .config/qtile/pyproject.toml Remove .config/qtile/themes/__pycache__/__init__.cpython-311.pyc Remove .config/qtile/themes/__pycache__/everforest.cpython-311.pyc Remove .config/qtile/themes/__init__.py Remove .config/qtile/themes/everforest.py Remove .config/qtile/ui/__init__.py Remove .config/qtile/ui/__pycache__/__init__.cpython-311.pyc Remove .config/qtile/ui/__pycache__/bars.cpython-311.pyc Remove .config/qtile/ui/__pycache__/screens.cpython-311.pyc Remove .config/qtile/ui/bars.py Remove .config/qtile/ui/screens.py

--- private_dot_config/qtile/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-from core.helper import load_module
-from options import colorscheme
-from ui.screens import screens
-
-colorscheme_module_path = f"themes.{colorscheme}"
-
-colors = load_module(colorscheme_module_path)
-
-__all__ = ("colors", "screens")
--- private_dot_config/qtile/ui/screens.py
+++
@@ -1,4 +0,0 @@
-from libqtile.config import Screen
-from ui.bars import top_bar
-
-screens = [Screen(top=top_bar)]
