83d0157310158b3476aa21fcd211cecdbcc00500
Francisco Suarez <franksg88@gmail.com>
2024-03-19 09:13:03 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/nvim/lua/plugins/executable_colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/executable_colorscheme.lua
@@ -1,21 +1,18 @@
 return {
-  "neanias/everforest-nvim",
+  "rose-pine/neovim",
+  name = "rose-pine",
   version = false,
   lazy = false,
   priority = 1000,
   opts = {
-    background = "soft",
-    italics = true,
-    on_highlights = function(hl, palette)
-      hl.DiagnosticError = { fg = palette.none, bg = palette.none, sp = palette.red }
-      hl.DiagnosticWarn = { fg = palette.none, bg = palette.none, sp = palette.yellow }
-      hl.DiagnosticInfo = { fg = palette.none, bg = palette.none, sp = palette.blue }
-      hl.DiagnosticHint = { fg = palette.none, bg = palette.none, sp = palette.green }
-    end,
+    variant = "moon",
+    dark_variant = "moon",
   },
   config = function(_, opts)
-    local theme = require("everforest")
+    local theme = require("rose-pine")
+
     theme.setup(opts)
-    theme.load()
+    vim.cmd("colorscheme rose-pine-moon")
+    -- theme.load()
   end,
 }
