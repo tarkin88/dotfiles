25154fdab47747bb3f0304245c8e3ebd9eb6e533
Francisco Suarez <franksg88@gmail.com>
2024-11-15 12:30:15 -0600

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,6 @@ set fish_greeting
 bind \cr _fzf_search_history
 bind -M insert \cr _fzf_search_history
 zoxide init fish | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/frank/.local/share/google-cloud-sdk/path.fish.inc' ]; . '/home/frank/.local/share/google-cloud-sdk/path.fish.inc'; end
