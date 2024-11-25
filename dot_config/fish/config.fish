f6beb71dd84de957417724ef2e299803c427539e
Francisco Suarez <franksg88@gmail.com>
2024-11-25 17:04:06 -0600

    Add .config/fish/completions/fisher.fish Add .config/fish/completions/fzf_configure_bindings.fish Add .config/fish/completions/nvm.fish Add .config/fish/conf.d/fzf.fish Add .config/fish/conf.d/nvm.fish Add .config/fish/conf.d/zoxide.fish Add .config/fish/config.fish Add .config/fish/config.fish.backup Add .config/fish/fish_plugins Add .config/fish/fish_variables Add .config/fish/functions/_fzf_configure_bindings_help.fish Add .config/fish/functions/_fzf_extract_var_info.fish Add .config/fish/functions/_fzf_preview_changed_file.fish Add .config/fish/functions/_fzf_preview_file.fish Add .config/fish/functions/_fzf_report_diff_type.fish Add .config/fish/functions/_fzf_report_file_type.fish Add .config/fish/functions/_fzf_search_directory.fish Add .config/fish/functions/_fzf_search_git_log.fish Add .config/fish/functions/_fzf_search_git_status.fish Add .config/fish/functions/_fzf_search_history.fish Add .config/fish/functions/_fzf_search_processes.fish Add .config/fish/functions/_fzf_search_variables.fish Add .config/fish/functions/_fzf_wrapper.fish Add .config/fish/functions/_nvm_index_update.fish Add .config/fish/functions/_nvm_list.fish Add .config/fish/functions/_nvm_version_activate.fish Add .config/fish/functions/_nvm_version_deactivate.fish Add .config/fish/functions/cat.fish Add .config/fish/functions/fetch.fish Add .config/fish/functions/fish_prompt.fish Add .config/fish/functions/fisher.fish Add .config/fish/functions/fzf_configure_bindings.fish Add .config/fish/functions/ga.fish Add .config/fish/functions/gc.fish Add .config/fish/functions/gco.fish Add .config/fish/functions/nvm.fish Add .config/fish/functions/state.fish Add .config/fish/functions/zz.fish Add .config/fish/themes/.keep

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+bind \cr _fzf_search_history
+bind -M insert \cr _fzf_search_history
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/frank/.local/share/google-cloud-sdk/path.fish.inc' ]
+    . '/home/frank/.local/share/google-cloud-sdk/path.fish.inc'
+end
