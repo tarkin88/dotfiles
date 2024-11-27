1b2d19d7e4b1c256fbb71d81d957c2108f8bcd23
Francisco Suarez <franksg88@gmail.com>
2024-11-27 15:46:39 -0600

    Update .config/fish/conf.d/zoxide.fish Update .config/fish/config.fish Update .config/fish/config.fish.backup

--- dot_config/fish/conf.d/zoxide.fish
+++ dot_config/fish/conf.d/zoxide.fish
@@ -1,2 +1,6 @@
 set -Ux _ZO_EXCLUDE_DIRS "/home/frank/.local/share/*"
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/frank/.local/share/google-cloud-sdk/path.fish.inc' ]
+    . '/home/frank/.local/share/google-cloud-sdk/path.fish.inc'
+end
 zoxide init fish | source
