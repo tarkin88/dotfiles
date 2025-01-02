2f843a463addad17f8e1d1508ecdaf33518a6c48
Francisco Suarez <franksg88@gmail.com>
2025-01-02 17:49:21 -0600

    Add .config/fish/completions/fisher.fish Add .config/fish/completions/fzf_configure_bindings.fish Add .config/fish/conf.d/cargo.fish Add .config/fish/conf.d/done.fish Add .config/fish/conf.d/fzf.fish Add .config/fish/conf.d/rustup.fish Add .config/fish/conf.d/zoxide.fish Add .config/fish/config.fish Add .config/fish/fish_plugins Add .config/fish/fish_variables Add .config/fish/functions/_fzf_configure_bindings_help.fish Add .config/fish/functions/_fzf_extract_var_info.fish Add .config/fish/functions/_fzf_preview_changed_file.fish Add .config/fish/functions/_fzf_preview_file.fish Add .config/fish/functions/_fzf_report_diff_type.fish Add .config/fish/functions/_fzf_report_file_type.fish Add .config/fish/functions/_fzf_search_directory.fish Add .config/fish/functions/_fzf_search_git_log.fish Add .config/fish/functions/_fzf_search_git_status.fish Add .config/fish/functions/_fzf_search_history.fish Add .config/fish/functions/_fzf_search_processes.fish Add .config/fish/functions/_fzf_search_variables.fish Add .config/fish/functions/_fzf_wrapper.fish Add .config/fish/functions/cat.fish Add .config/fish/functions/fetch.fish Add .config/fish/functions/fish_prompt.fish.bak Add .config/fish/functions/fisher.fish Add .config/fish/functions/fzf_configure_bindings.fish Add .config/fish/functions/ga.fish Add .config/fish/functions/gc.fish Add .config/fish/functions/gco.fish Add .config/fish/functions/state.fish Add .config/fish/functions/zz.fish Add .config/fish/themes/.keep

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+bind \cr _fzf_search_history
+bind -M insert \cr _fzf_search_history
+
+starship init fish | source
---
+++ dot_config/fish/functions/fetch.fish
@@ -0,0 +1,4 @@
+function fetch --wraps='macchina -t Lithium' --description 'alias fetch=macchina -t Lithium'
+  macchina -t Lithium $argv
+        
+end
