7d3eb06bbd5f7a66905ffd44e9e84ea4e6155682
Francisco Suarez <franksg88@gmail.com>
2025-06-27 08:51:53 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua Update .config/nvim/lua/plugins/dap.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,31 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"rebelot/kanagawa.nvim",
 	version = false,
 	lazy = false,
 	priority = 1000,
 	opts = {
-		variant = "dawn",
+		{
+			compile = true, -- enable compiling the colorscheme
+			undercurl = true, -- enable undercurls
+			commentStyle = { italic = true },
+			functionStyle = { bold = true },
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = false, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+			theme = "wave", -- Load "wave" theme
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+			},
+		},
 	},
 	config = function(_, opts)
-		local theme = require("rose-pine")
+		local theme = require("kanagawa")
 		theme.setup(opts)
-		-- theme.load()
+		theme.load()
 
-		vim.cmd("colorscheme rose-pine-dawn")
+		-- vim.cmd("colorscheme kanagawa")
 	end,
 }
